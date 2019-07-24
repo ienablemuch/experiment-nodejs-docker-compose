35f6eb3f0f255388ef9acca7d1a097da73b5bf05
dev <me@ienablemuch.com>
2019-07-24 12:54:25 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+
+  const message = `Hello world: ${process.env.GREETING_MESSAGE}`;
+  res.send(message);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
